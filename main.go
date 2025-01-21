7a43319c06f6eba8211a5593ffa7dae3d42095b3
Anuchat  Rungsiyanon <rungsiyanon@gmail.com>
2025-01-21 10:32:50 +0700

    210120251032

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Information Technology")
+// }
+
 package main
 
-import "fmt"
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Information Technology")
+  router := gin.Default()
+  //Default API Method
+  router.GET("/", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "API SERVER STATUS OK!",
+    })
+  })
+
+  //Employee API Method
+  router.GET("/employee", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "Employee GET Method!",
+    })
+  })
+
+  router.POST("/employee", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "Employee POST Method!",
+    })
+  })
+
+  router.PUT("/employee", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "Employee PUT Method!",
+    })
+  })
+
+  router.DELETE("/employee", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "Employee DELETE Method!",
+    })
+  })
+
+  router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
